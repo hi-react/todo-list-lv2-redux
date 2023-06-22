52e1deffb68140a6744cd0cc215cb7980dd6b529
heeyeon <godeeyeon@gmail.com>
2023-06-22 12:38:19 +0900

    feat. todo-list 상세 페이지: useParams로 route의 id 가져와서 todo state 정보 뿌려줌

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+
+function DetailPage() {
+  // store에서 todo 데이터 가져오기
+  const { todo } = useSelector((state) => state.todo);
+
+  // Router에서 URL parameter인 id값 가져오기
+  const params = useParams();
+
+  // todo 돌면서 params로 가져온 id값에 해당하는 todo 찾기(find 메서드)
+  const foundTodo = todo.find((todoItem) => {
+    return todoItem.id === params.id;
+  });
+
+  return (
+    <StLayout>
+      <StTodoItem key={foundTodo.id}>
+        <StNav>
+          <div>id: {foundTodo.id}</div>
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#5b92dc;",
+            }}
+          >
+            이전으로
+          </Link>
+        </StNav>
+        <StTodoItemInfo>
+          <StTodoItemTitle>{foundTodo.title}</StTodoItemTitle> <br />
+          <StTodoItemContent>{foundTodo.content}</StTodoItemContent>
+        </StTodoItemInfo>
+      </StTodoItem>
+    </StLayout>
+  );
+}
+
+export default DetailPage;
+
+// styled-components
+const StLayout = styled.section`
+  max-width: 1200px;
+  min-width: 800px;
+
+  margin: auto;
+`;
+
+const StTodoItem = styled.div`
+  width: 700px;
+  margin: 100px auto;
+  padding: 50px;
+
+  background-color: #e5eefa;
+  border: 3px solid #5b92dc;
+
+  border-radius: 20px;
+`;
+
+const StNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StTodoItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+
+  padding-top: 40px;
+
+  font-size: 16px;
+  color: black;
+`;
+
+const StTodoItemTitle = styled.h3`
+  margin: 0px;
+  padding: 0px;
+  font-size: 20px;
+`;
+
+const StTodoItemContent = styled.p`
+  margin-top: 0px;
+`;
