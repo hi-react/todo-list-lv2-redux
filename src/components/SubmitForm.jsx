e432d8049e0c4980ccf0c34ad40aae6166963c1f
heeyeon <godeeyeon@gmail.com>
2023-06-22 12:36:57 +0900

    feat. todo-list 메인에서 사용할 컴포넌트 SubmitForm과 TodoList (Store의 todo state / styled-components 이용)

---
+++ src/components/SubmitForm.jsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// action creator import
+import { addTodo } from "../redux/modules/todo";
+import { styled } from "styled-components";
+import { v4 as uuidv4 } from "uuid";
+
+const SubmitForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // store에서 값 가져오기
+  const todo = useSelector((state) => state.todo);
+  console.log(todo);
+
+  // dispatch도 가져와야지
+  const dispatch = useDispatch();
+
+  // Add 버튼
+  const addTodoItemHandler = (event) => {
+    event.preventDefault();
+    // return을 해주지 않으면, 모두 입력해달라는 알람이 뜨고도 빈 카드를 그대로 붙여넣음!
+    if (title.trim() === "" || content.trim() === "") {
+      alert("제목과 내용을 모두 입력해주세요.");
+      setTitle("");
+      setContent("");
+      return;
+    }
+
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        title,
+        content,
+        isDone: false,
+      })
+    );
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <StAddForm onSubmit={addTodoItemHandler}>
+      <label>제목: </label>
+      <StInputTitle
+        value={title}
+        placeholder="제목을 입력해주세요."
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      &nbsp;
+      <label>내용: </label>
+      <StInputContent
+        value={content}
+        placeholder="내용을 입력해주세요."
+        onChange={(event) => setContent(event.target.value)}
+      />
+      <StButton>추가하기</StButton>
+    </StAddForm>
+  );
+};
+
+export default SubmitForm;
+
+// styled-components
+const StAddForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 120px;
+
+  padding: 0px 35px;
+
+  background-color: #d8e5f7;
+
+  border: 1px solid gray;
+  border-radius: 10px;
+`;
+
+const StInputTitle = styled.input`
+  height: 40px;
+  width: 270px;
+
+  padding: 0 12px;
+
+  border: 1px solid rgb(182, 161, 161);
+  border-radius: 4px;
+`;
+
+const StInputContent = styled.input`
+  height: 40px;
+  width: 600px;
+
+  padding: 0 12px;
+
+  border: 1px solid rgb(182, 161, 161);
+  border-radius: 4px;
+`;
+
+const StButton = styled.button`
+  height: 40px;
+  width: 110px;
+
+  margin-right: 10px;
+
+  border: 1px solid gray;
+  border-radius: 4px;
+
+  font-weight: 700;
+  font-size: 16px;
+  color: black;
+  cursor: pointer;
+
+  // hover는 어떻게 하징........
+`;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,139 @@
+import { styled } from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo } from "../redux/modules/todo";
+import { stateChangeTodo } from "../redux/modules/todo";
+import { Link } from "react-router-dom";
+
+const TodoList = ({ isDone }) => {
+  // store에서 todo 데이터 가져오기
+  const { todo } = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <StTodoMenu>{isDone ? "완료한 일 🛠️" : "해야할 일 ✍🏻"}</StTodoMenu>
+      <StTodoList>
+        {todo
+          .filter((todoItem) => todoItem.isDone === isDone)
+          .map(({ id, title, content }) => {
+            return (
+              <StTodoItem key={id}>
+                <Link
+                  to={`/detail/${id}`}
+                  style={{
+                    textDecoration: "none",
+                    color: "#1174ad",
+                  }}
+                >
+                  상세보기
+                </Link>
+                <StTodoItemInfo>
+                  <StTodoItemTitle>{title}</StTodoItemTitle> <br />
+                  <StTodoItemContent>{content}</StTodoItemContent>
+                </StTodoItemInfo>
+                <StButtonSet>
+                  <StStateChangeButton
+                    onClick={() => {
+                      dispatch(stateChangeTodo(id));
+                    }}
+                  >
+                    {isDone ? "취소" : "완료"}
+                  </StStateChangeButton>
+                  <StDeleteButton
+                    onClick={() => {
+                      dispatch(deleteTodo(id));
+                    }}
+                  >
+                    삭제
+                  </StDeleteButton>
+                </StButtonSet>
+              </StTodoItem>
+            );
+          })}
+      </StTodoList>
+    </>
+  );
+};
+
+export default TodoList;
+
+// styled-components
+const StTodoMenu = styled.h1`
+  margin: 25px 30px;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const StTodoList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const StTodoItem = styled.div`
+  /* height: 180px; */
+  width: 280px;
+  margin: 10px 10px 10px 10px;
+  padding: 20px;
+  background-color: #e5eefa;
+  /* border: 3px solid #5b92dc; */
+  border: 3px solid #a0a2a5;
+
+  border-radius: 20px;
+`;
+
+const StTodoItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+
+  padding-top: 40px;
+
+  font-size: 16px;
+  color: black;
+`;
+
+const StTodoItemTitle = styled.h3`
+  margin: 0px;
+  padding: 0px;
+  font-size: 20px;
+`;
+
+// 내용이 길어지면 줄바꿈 되도록
+const StTodoItemContent = styled.p`
+  margin-top: 0px;
+  word-break: break-word;
+`;
+
+const StButtonSet = styled.div`
+  gap: 10px;
+
+  display: flex;
+  justify-content: center;
+
+  margin-top: 20px;
+`;
+
+const StStateChangeButton = styled.button`
+  height: 30px;
+  width: 40%;
+
+  background-color: #f4f7fc;
+
+  border: 2px solid #216acd;
+  border-radius: 5px;
+  cursor: pointer;
+
+  // hover...............
+`;
+
+const StDeleteButton = styled.button`
+  height: 30px;
+  width: 40%;
+
+  background-color: #f4f7fc;
+
+  border: 2px solid red;
+  border-radius: 5px;
+  cursor: pointer;
+`;
